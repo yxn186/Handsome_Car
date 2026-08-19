6427da4d250a1020661aee967fef6ead87ac0b41
yxn186 <1412535486@qq.com>
2026-08-19 16:02:43 +0800

    添加底盘库的头文件和实现文件

---
+++ Usercode/Application/App_Chassis.h
@@ -0,0 +1,34 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    App_Chassis.h
+  * @brief   This file contains all the function prototypes for
+  *          the App_Chassis.c/.cpp file
+  * @author  yxn
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __APP_CHASSIS_H__
+#define __APP_CHASSIS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/*YOUR CODE*/
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __APP_CHASSIS_H__ */

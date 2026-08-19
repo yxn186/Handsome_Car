3902df24c94f7163e6bcde7fca47ecb0c48df24b
yxn186 <1412535486@qq.com>
2026-08-19 16:50:53 +0800

    feat: Enable USART3 and DMA for UART communication
    - Updated stm32f4xx_hal_conf.h to enable HAL_UART_MODULE.
    - Modified stm32f4xx_it.h to declare USART3 and DMA1 interrupt handlers.
    - Enhanced gpio.c to enable GPIOC clock for USART3.
    - Updated main.c to initialize DMA and USART3 peripherals.
    - Implemented DMA1_Stream1_IRQHandler and USART3_IRQHandler in stm32f4xx_it.c.
    - Configured DMA for USART3 reception in Handsome_Car.ioc.
    - Added dma.h and usart.h headers for DMA and USART3 functionalities.
    - Created dma.c and usart.c files to handle DMA and USART3 initialization and configuration.
    - Updated CMakeLists.txt to include new DMA and USART source files.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,8 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "can.h"
+#include "dma.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -89,8 +91,10 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_CAN1_Init();
   MX_CAN2_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
